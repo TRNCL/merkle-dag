dfcb1e2544499f9c0d3cae3de5678f97707c6964
TRNCL <94829054+TRNCL@users.noreply.github.com>
2024-05-10 16:35:35 +0800

    Add files via upload

--- dag.go
+++ dag.go
@@ -1,6 +1,11 @@
 package merkledag
 
-import "hash"
+import (
+	"hash"
+	"bytes"
+	"encoding/gob"
+	"io"
+)
 
 type Link struct {
 	Name string
@@ -13,7 +18,75 @@ type Object struct {
 	Data  []byte
 }
 
-func Add(store KVStore, node Node, hp HashPool) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+func Add(store KVStore, node Node, hp HashPool) ([]byte, error) {
+	// 创建哈希对象
+	h := hp.Get()
+
+	// 处理文件节点
+	if node.Type() == FILE {
+		file := node.(File) // 类型断言为File
+		data := file.Bytes()
+
+		// 将文件数据写入哈希对象
+		h.Write(data)
+
+		// 将文件数据存储到KVStore
+		err := store.Put(h.Sum(nil), data)
+		if err != nil {
+			return nil, err
+		}
+
+		// 返回文件内容的哈希作为Merkle Root
+		return h.Sum(nil), nil
+	}
+
+	// 处理目录节点
+	if node.Type() == DIR {
+		dir := node.(Dir) // 类型断言为Dir
+		iterator := dir.It()
+
+		var links []Link
+
+		// 遍历目录中的所有子节点
+		for iterator.Next() {
+			child := iterator.Node()
+			childHash, err := Add(store, child, hp)
+			if err != nil {
+				return nil, err
+			}
+
+			// 构建子节点的Link信息
+			link := Link{
+				Name: child.Name(),
+				Hash: childHash,
+				Size: int(child.Size()),
+			}
+			links = append(links, link)
+		}
+
+		// 创建Object并序列化
+		object := Object{
+			Links: links,
+		}
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		err := enc.Encode(object)
+		if err != nil {
+			return nil, err
+		}
+
+		// 将Object数据写入哈希对象
+		io.Copy(h, &buf)
+
+		// 将Object数据存储到KVStore
+		err = store.Put(h.Sum(nil), buf.Bytes())
+		if err != nil {
+			return nil, err
+		}
+
+		// 返回Object数据的哈希作为Merkle Root
+		return h.Sum(nil), nil
+	}
+
+	return nil, fmt.Errorf("unknown node type: %d", node.Type())
 }
--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,52 @@
 package merkledag
 
-// Hash to file
-func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"path/filepath"
+	"strings"
+)
+
+// Hash2File 函数接收一个KVStore和一个哈希值，以及一个文件路径，返回该文件的内容。
+func Hash2File(store KVStore, hash []byte, path string, hp HashPool) ([]byte, error) {
+	// 从KVStore中获取数据
+	data, err := store.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解析路径
+	dir, file := filepath.Split(path)
+	if dir == "" {
+		dir = "."
+	}
+
+	// 检查数据是否为目录
+	var obj Object
+	dec := gob.NewDecoder(bytes.NewReader(data))
+	err = dec.Decode(&obj)
+	if err == nil {
+		// 数据为目录，解析Links
+		for _, link := range obj.Links {
+			if link.Name == file {
+				// 如果是当前路径的文件，递归调用
+				if dir == "." {
+					return Hash2File(store, link.Hash, ".", hp)
+				} else {
+					return Hash2File(store, link.Hash, dir, hp)
+				}
+			}
+		}
+		// 如果没有找到对应的文件，返回错误
+		return nil, fmt.Errorf("file not found in directory: %s", path)
+	}
+
+	// 如果数据不是目录，且路径是当前目录，则返回数据
+	if dir == "." && file == "" {
+		return data, nil
+	}
+
+	// 如果数据不是目录，但路径不是当前目录，返回错误
+	return nil, fmt.Errorf("file not found: %s", path)
 }
